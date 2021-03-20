5564a25c36a2c6973eb3144616d8ace60636f004
Grasuggan <32665572+Grasuggan@users.noreply.github.com>
2021-03-20 10:30:38 +0100

    create react app

---
+++ labb1/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Here comes our code</p>
+    </div>
+  );
+}
+
+export default App;
